2fc3199343b46eaa7fda5480c1ce88e33688df84
Davi Severino Oliveira de Souza <daviseverinoinf2@gmail.com>
2026-09-14 08:38:03 -0300

    teste = false;

--- index.php
+++ index.php
@@ -1,49 +1,10 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link rel="stylesheet" href="../assets/styles/style.css">
-    <link rel="stylesheet" href="../assets/styles/responsividade.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
-    <link rel="icon" type="image/png" href="img/logo 40 anos.png">
-
-    <title>Tabela Periódica</title>
-</head>
+<?php include ('includes/head.html'); ?>
 
 <body>
 
-<header>
-    <div class="header_container">
-
-        <a href="#" id="logo">
-            <img src="img/logo 40 anos.png" alt="logo 40 anos mlmb" style="width: 175px; height: auto; padding: 0px;">
-        </a>
-
-        <h2><a href="#main">TABELA PERIÓDICA </a><span style="color: #923F84; cursor: pointer; text-decoration: none; color: black;"><a href="http://instagram.com/flashnewsmlmb" target="_blank">MARIA LUIZA MIRANDA BASTOS</a></span></h2>
-
-        <div class="menu-area">
-            <button id="buttonTableP">MUDAR TABELA</button>
-            <button id="buttonMenu">☰</button>
-
-            <ul id="menuDropdown">
-                <li><a href="#" data-classe="metais_alcalinos">Metais Alcalinos</a></li>
-                <li><a href="#" data-classe="metais_alcalinos_terrosos">Metais Alcalinos-Terrosos</a></li>
-                <li><a href="#" data-classe="familia_do_boro">Família do Boro</a></li>
-                <li><a href="#" data-classe="familia_do_carbono">Família do Carbono</a></li>
-                <li><a href="#" data-classe="familia_do_nitrogenio">Família do Nitrogênio</a></li>
-                <li><a href="#" data-classe="calcogenios">Calcogenios</a></li>
-                <li><a href="#" data-classe="nao_metais_diatomicos">Não Metais Diatômicos</a></li>
-                <li><a href="#" data-classe="gases_nobres">Gases Nobres</a></li>
-                <li><a href="#" data-classe="desconhecidos">Desconhecidos</a></li>
-                <li><a href="#" data-classe="lantanideos">Lantanídeos</a></li>
-                <li><a href="#" data-classe="actinideos">Actinídeos</a></li>
-            </ul>
-        </div>
-
-    </div>
-</header>
+<?php include ('includes/header.html'); ?>
 
 <div class="search-container">
 
@@ -1044,46 +1005,7 @@
 
 
 
-<footer class="footer_container">
-
-    <div class="footer_conteudo">
-
-        <div class="footer_principal">
-            <h2>Tabela Periódica MLMB</h2>
-            <p>
-                Projeto desenvolvido para fins educacionais,
-                aprendizado e apresentação na Feira do Conhecimento.
-            </p>
-        </div>
-
-        <div class="footer_info">
-            <div class="footer_item">
-                <span>Desenvolvimento</span>
-                <strong>Davi Severino Oliveira de Souza</strong>
-                <strong>Victor Leandro Oliveira Dias</strong>
-            </div>
-
-            <div class="footer_item">
-                <span>Orientador</span>
-                <strong>Joel Teixeira da Cunha</strong>
-            </div>
-
-            <div class="footer_item">
-                <span>Turma</span>
-                <strong>3° Informática — 2026</strong>
-            </div>
-        </div>
-
-        <div class="footer_linha"></div>
-
-        <div class="footer_final">
-            <span>Feira do Conhecimento • 2026</span>
-            <span>Aprendizado • Ciência • Educação</span>
-        </div>
-
-    </div>
-
-</footer>
+<?php include ('includes/footer.html');?>
 
 
 <script src="script.js"></script>
